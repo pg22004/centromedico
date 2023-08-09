56d788514520a4b076cbc5aae7e9dc5bff989717
NestorEstupinian <em22001@esfe.edu.agape.edu.sv>
2023-08-09 16:16:04 -0600

    Programacion de las clases Paciente y Historial

--- CentroMedico.EntidadesDeNegocios/src/centromedico/entidadesdenegocios/Historial.java
+++ CentroMedico.EntidadesDeNegocios/src/centromedico/entidadesdenegocios/Historial.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package centromedico.entidadesdenegocios;
 
-/**
- *
- * @author pc
- */
+import java.time.LocalDate;
+
+
 public class Historial {
+    
+    private int id;
+    private LocalDate fechaEntrada;
+    private String detalleRegistro;
+
+    public Historial() {
+    }
+
+    public Historial(int id, LocalDate fechaEntrada, String detalleRegistro) {
+        this.id = id;
+        this.fechaEntrada = fechaEntrada;
+        this.detalleRegistro = detalleRegistro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(LocalDate fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getDetalleRegistro() {
+        return detalleRegistro;
+    }
+
+    public void setDetalleRegistro(String detalleRegistro) {
+        this.detalleRegistro = detalleRegistro;
+    }
+    
     
 }
--- CentroMedico.EntidadesDeNegocios/src/centromedico/entidadesdenegocios/Paciente.java
+++ CentroMedico.EntidadesDeNegocios/src/centromedico/entidadesdenegocios/Paciente.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package centromedico.entidadesdenegocios;
 
-/**
- *
- * @author pc
- */
+import java.time.LocalDate;
+
+
 public class Paciente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaRegistro;
+
+    public Paciente() {
+    }
+
+    public Paciente(int Id, String nombre, String apellido, LocalDate FechaRegistro) {
+        this.id = Id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaRegistro = FechaRegistro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int Id) {
+        this.id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate FechaRegistro) {
+        this.fechaRegistro = FechaRegistro;
+    }
     
 }
