c8da826d2c5eda352ba9cd408f082e84589999b7
Iliana <pg22004@esfe.agape.edu.sv>
2023-08-16 01:23:28 +0200

    Codificacion de la clase PacienteDAL

--- CentroMedico.AccesoADatos/src/centromedico/accesoadatos/PacienteDAL.java
+++ CentroMedico.AccesoADatos/src/centromedico/accesoadatos/PacienteDAL.java
@@ -21,14 +21,14 @@ public class PacienteDAL {
     }
     
      private static String agregarOrderBy(Paciente pPaciente) {
-        String sql = " ORDER BY c.Id DESC";
+        String sql = " ORDER BY p.Id DESC";
         if (pPaciente.getTop_aux() > 0 && ComunDB.TIPODB == ComunDB.TipoDB.MYSQL) {
             sql += " LIMIT " + pPaciente.getTop_aux() + " ";
         }
         return sql;
     }
   
-       public static int crear(Paciente pPaciente) throws Exception {
+      public static int crear(Paciente pPaciente) throws Exception {
         int result;
         String sql;
         try (Connection conn = ComunDB.obtenerConexion();) { 
@@ -88,5 +88,131 @@ public class PacienteDAL {
             throw ex;
         }
         return result;
+    }
+         static int asignarDatosResultSet(Paciente pPaciente, ResultSet pResultSet, int pIndex) throws Exception {
+        pIndex++;
+        pPaciente.setId(pResultSet.getInt(pIndex));
+        pIndex++;
+        pPaciente.setNombre(pResultSet.getString(pIndex));
+        pIndex++;
+        pPaciente.setApellido(pResultSet.getString(pIndex));
+        pIndex++;
+        pPaciente.setFechaRegistro(pResultSet.getDate(pIndex).toLocalDate()); 
+        return pIndex;
+    }
+         
+        private static void obtenerDatos(PreparedStatement pPS, ArrayList<Paciente> pPaciente) throws Exception {
+        try (ResultSet resultSet = ComunDB.obtenerResultSet(pPS);) {
+            while (resultSet.next()) {
+                Paciente paciente = new Paciente(); 
+                asignarDatosResultSet(paciente, resultSet, 0);
+                pPaciente.add(paciente);
+            }
+            resultSet.close();
+        } catch (SQLException ex) {
+            throw ex;
+        }
+    }
+        
+        public static Paciente obtenerPorId(Paciente pPaciente) throws Exception {
+        Paciente paciente = new Paciente();
+        ArrayList<Paciente> pacientes = new ArrayList();
+        try (Connection conn = ComunDB.obtenerConexion();) { 
+            String sql = obtenerSelect(pPaciente);
+            sql += " WHERE p.Id=?";
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) {
+                ps.setInt(1, pPaciente.getId());
+                obtenerDatos(ps, pacientes);
+                ps.close();
+            } catch (SQLException ex) {
+                throw ex;
+            }
+            conn.close();
+        }
+        catch (SQLException ex) {
+            throw ex;
+        }
+        if (pacientes.size() > 0) {
+            paciente = pacientes.get(0);
+        }
+        return paciente;
+    }
+        
+        public static ArrayList<Paciente> obtenerTodos() throws Exception {
+        ArrayList<Paciente> pacientes = new ArrayList<>();
+        try (Connection conn = ComunDB.obtenerConexion();) {
+            String sql = obtenerSelect(new Paciente());
+            sql += agregarOrderBy(new Paciente());
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) {
+                obtenerDatos(ps, pacientes);
+                ps.close();
+            } catch (SQLException ex) {
+                throw ex;
+            }
+            conn.close();
+        } 
+        catch (SQLException ex) {
+            throw ex;
+        }
+        return pacientes;
+    }
+        
+        static void querySelect(Paciente pPaciente, ComunDB.utilQuery pUtilQuery) throws SQLException {
+        PreparedStatement statement = pUtilQuery.getStatement();
+        if (pPaciente.getId() > 0) {
+            pUtilQuery.AgregarNumWhere(" p.Id=? ");
+            if (statement != null) { 
+                statement.setInt(pUtilQuery.getNumWhere(), pPaciente.getId()); 
+            }
+        }
+
+        if (pPaciente.getNombre() != null && pPaciente.getNombre().trim().isEmpty() == false) {
+            pUtilQuery.AgregarNumWhere(" p.Nombre LIKE ? "); 
+            if (statement != null) {
+                statement.setString(pUtilQuery.getNumWhere(), "%" + pPaciente.getNombre() + "%"); 
+            }
+        }
+        
+        if (pPaciente.getApellido()!= null && pPaciente.getApellido().trim().isEmpty() == false) {
+            pUtilQuery.AgregarNumWhere(" p.Apellido LIKE ? "); 
+            if (statement != null) {
+                statement.setString(pUtilQuery.getNumWhere(), "%" + pPaciente.getApellido()+ "%"); 
+            }
+        }
+        
+//        if (pPaciente.getFechaRegistro()!= null && pPaciente.getFechaRegistro().trim().isEmpty() == false) {
+//            pUtilQuery.AgregarNumWhere(" p.FechaRegistro LIKE ? "); 
+//            if (statement != null) {
+//                statement.setString(pUtilQuery.getNumWhere(), "%" + pPaciente.getFechaRegistro()+ "%"); 
+//            }
+//        }
+              
+    }
+        
+        public static ArrayList<Paciente> buscar(Paciente pPaciente) throws Exception {
+        ArrayList<Paciente> pacientes = new ArrayList();
+        try (Connection conn = ComunDB.obtenerConexion();) {
+            String sql = obtenerSelect(pPaciente);
+            ComunDB comundb = new ComunDB();
+            ComunDB.utilQuery utilQuery = comundb.new utilQuery(sql, null, 0); 
+            querySelect(pPaciente, utilQuery);
+            sql = utilQuery.getSQL(); 
+            sql += agregarOrderBy(pPaciente);
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) {
+                utilQuery.setStatement(ps);
+                utilQuery.setSQL(null);
+                utilQuery.setNumWhere(0); 
+                querySelect(pPaciente, utilQuery);
+                obtenerDatos(ps, pacientes);
+                ps.close();
+            } catch (SQLException ex) {
+                throw ex;
+            }
+            conn.close();
+        }
+        catch (SQLException ex) {
+            throw ex;
+        }
+        return pacientes;
     }
 }
