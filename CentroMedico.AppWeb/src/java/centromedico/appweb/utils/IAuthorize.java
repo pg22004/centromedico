033ba7c1f3a778af022e761693de4c4707d41dc6
Iliana <pg22004@esfe.agape.edu.sv>
2023-08-18 02:46:22 +0200

    Estructura de archivos del proyecto AppWeb

---
+++ CentroMedico.AppWeb/src/java/centromedico/appweb/utils/IAuthorize.java
@@ -0,0 +1,8 @@
+package centromedico.appweb.utils;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
+public interface IAuthorize {
+    void authorize() throws ServletException, IOException;
+}
