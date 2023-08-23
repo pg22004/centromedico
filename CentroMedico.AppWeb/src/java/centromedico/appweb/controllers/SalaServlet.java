82ad6c56e8349d0bcdedf273c776e62fd969ccad
Yanira <oc22002@esfe.agape.edu.sv>
2023-08-22 22:57:42 -1200

    Codificación del controlador Sala

--- CentroMedico.AppWeb/src/java/centromedico/appweb/controllers/SalaServlet.java
+++ CentroMedico.AppWeb/src/java/centromedico/appweb/controllers/SalaServlet.java
@@ -1,15 +1,202 @@
 package centromedico.appweb.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import centromedico.accesoadatos.SalaDAL;
+import centromedico.entidadesdenegocios.Sala;
+import centromedico.appweb.utils.*;
 
 @WebServlet(name = "SalaServlet", urlPatterns = {"/Sala"})
 public class SalaServlet extends HttpServlet {
 
+    private Sala obtenerSala(HttpServletRequest request) {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        Sala sala = new Sala();
+        if (accion.equals("create") == false) {
+            sala.setId(Integer.parseInt(Utilidad.getParameter(request, "id", "0")));
+        }
+
+        sala.setNombre(Utilidad.getParameter(request, "nombre", ""));
+        sala.setNumeroCamas(Integer.parseInt(Utilidad.getParameter(request, "numeroCamas", "0")));
+        if (accion.equals("index")) {
+            sala.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            sala.setTop_aux(sala.getTop_aux() == 0 ? Integer.MAX_VALUE : sala.getTop_aux());
+        }
+        return sala;
+    }
+    
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = new Sala();
+            sala.setTop_aux(10);
+            ArrayList<Sala> salas = SalaDAL.buscar(sala);
+            request.setAttribute("salas", salas);
+            request.setAttribute("top_aux", sala.getTop_aux());             
+            request.getRequestDispatcher("Views/Sala/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = obtenerSala(request);
+            ArrayList<Sala> salas = SalaDAL.buscar(sala);
+            request.setAttribute("salas", salas);
+            request.setAttribute("top_aux", sala.getTop_aux());
+            request.getRequestDispatcher("Views/Sala/index.jsp").forward(request, response);
+        } catch (Exception ex) { 
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("Views/Sala/create.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = obtenerSala(request);
+            int result = SalaDAL.crear(sala);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro ingresar un nuevo registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = obtenerSala(request);
+            Sala sala_result = SalaDAL.obtenerPorId(sala);
+            if (sala_result.getId() > 0) {
+                request.setAttribute("sala", sala_result);
+            } else {
+                Utilidad.enviarError("El Id:" + sala.getId() + " no existe en la tabla de Sala", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Sala/edit.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = obtenerSala(request);
+            int result = SalaDAL.modificar(sala);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro actualizar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            // Enviar al jsp de error si hay un Exception
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Sala/details.jsp").forward(request, response);
+    }
+    
+    private void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Sala/delete.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Sala sala = obtenerSala(request);
+            int result = SalaDAL.eliminar(sala);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro eliminar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doPostRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doPostRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doPostRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doPostRequestDelete(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
+    }
+// </editor-fold>
 }
